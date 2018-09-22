6cf0f82270b03354dc79423a72871b31798bbb6a
Kristian Mandrup <kmandrup@gmail.com>
2018-09-22 21:37:34 +0200

    starting schema tests WIP

--- src/schema.test.js
+++ src/schema.test.js
@@ -1,6 +1,70 @@
 const {resolveSchema, propsToOutput} = require('./schema');
+const properties = {
+  id: {
+    type: "string",
+    generated: true,
+    unique: true,
+    required: true
+  },
+  "name": {
+    "description": "Name of the person",
+    "type": "string",
+    "graphql": {
+      "decorators": {
+        "connection": {
+          "name": "UserNames"
+        }
+      }
+    }
+  },
+  "age": {
+    "description": "Age of person",
+    "type": "integer",
+    required: true
+  },
+  "money": {
+    "description": "Money in pocket",
+    "type": "number"
+  },
+  "accounts": {
+    "description": "Bank accounts",
+    "type": "array",
+    "items": {
+      "$ref": "#/definitions/account"
+    }
+  },
+  "numberOfChildren": {
+    "description": "Children parented",
+    "type": "array",
+    "items": {
+      "type": "number",
+      "enum": [0, 1, 2]
+    }
+  },
+  "favoriteCoulor": {
+    "description": "Colors liked",
+    "name": "color",
+    "type": "string",
+    "items": {
+      "type": "number",
+      "enum": ["red", "green", "blue"]
+    }
+  },
+  "car": {
+    "description": "Car owned",
+    "type": "object",
+    "decorators": {
+      "client": true
+    },
+    "properties": {
+      name: {
+        type: "string"
+      }
+    }
+  }
+}
 
-describe('resolveSchema', () => {
+describe.skip('resolveSchema', () => {
   test('it resolves object', () => {
     expect(1).toBe(1)
   })
@@ -10,4 +74,21 @@ describe('resolveSchema', () => {
   })
 })
 
-describe('propsToOutput', () => {})
+describe.only('propsToOutput', () => {
+  const result = propsToOutput({properties})
+
+  test('props', () => {
+    const {props} = result
+    expect(props).toBeTruthy()
+  })
+
+  test('enums', () => {
+    const {enums} = result
+    expect(enums).toBeTruthy()
+  })
+
+  test('types', () => {
+    const {types} = result
+    expect(types).toBeTruthy()
+  })
+})
